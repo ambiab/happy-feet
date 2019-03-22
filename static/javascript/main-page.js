d63ec5b2b62f7f43b1dfdda81929990f7dde927e
ambiab <a.begum7@lse.ac.uk>
2019-03-22 17:01:34 +0000

    added menu js (needs extra work)

---
+++ static/javascript/main-page.js
@@ -0,0 +1,11 @@
+document.addEventListener('load', () => {
+    const menuButton = document.querySelector('.menubutton');
+    menuButton.addEventListener('click', () => {
+        const menu = document.querySelector('.menu');
+        if (menu.getElementsByClassName.display === 'block') {
+            menu.getElementsByClassName.display === 'none';
+        } else; {
+            menu.getElementsByClassName.display === 'block';
+        }
+    })
+})
