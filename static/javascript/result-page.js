e7f98f2bbdb4e29e9605cccd2f5b5c50f69ad130
Huong Nguyen <huongg1409@gmail.com>
2019-03-28 12:20:27 +0000

    update new-layout for result page
    to only show the images

--- static/javascript/result-page.js
+++ static/javascript/result-page.js
@@ -53,59 +53,13 @@ const mockData = [
 
 document.addEventListener('DOMContentLoaded', function(){ 
     mockData.forEach(data => {
-        
-        const listings = document.getElementById('listings');
         const results = document.getElementById('results');
 
-        function setActive(element, url) {
-            // this is to loop through all the item elements,
-            // and remove any 'active' class name
-            let siblings = listings.getElementsByClassName('item');
-            for (let i = 0; i < siblings.length; i++) {
-                siblings[i].className = siblings[i].className
-                .replace(/active/, '').replace(/\s\s*$/, '');
-            }
-            element.className += ' active';
+        const imageWrapper = results.appendChild(document.createElement('div'));
+        imageWrapper.className = 'image-wrapper';
 
-            let imagetag = results.getElementsByClassName('image-tag')[0];
-            imagetag.style.backgroundImage = `url(${url})`
-        }
-    
-        const item = listings.appendChild(document.createElement('div'));
-        item.className = 'item';
-    
-        const link = item.appendChild(document.createElement('a'));
-        link.href = '#';
-        link.className = 'title';
+        const imageTag = imageWrapper.appendChild(document.createElement('img'));
+        imageTag.src = data.url
 
-        link.innerHTML = data.name;
-        link.innerHTML += '<br /><small class="quiet">' + data.feature + '</small>';
-
-        link.onclick = function() {
-            setActive(item, data.url);
-        }
     });
 });
-
-// function get_photos(name) {
-//     const settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         // "url": "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=96c5a95ca1319d7a14dc65975b456376&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1",
-//         "url": `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=96c5a95ca1319d7a14dc65975b456376&text=${name}&tags=mountain&format=json&nojsoncallback=1`,
-//         "method": "GET",
-//         "headers": {}
-//     }
-    
-//     $.ajax(settings).done(function (data) {
-//         const imageData = data.photos.photo[0];
-//         const farmId = imageData.farm;
-//         const serverId = imageData.server;
-//         const id = imageData.id;
-//         const secret = imageData.secret;
-
-//         console.log('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>')
-
-//         $("#result").append('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>');
-//     });
-// }
