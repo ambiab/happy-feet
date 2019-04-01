ba8529981d47e936827b9d461b881fc4f34ca3bf
Huongg <32060364+Huongg@users.noreply.github.com>
2019-04-01 21:25:12 +0100

    limit query search by using colheight

--- project/utils/data_config.py
+++ project/utils/data_config.py
@@ -3,27 +3,27 @@ from project.settings import FLIRCK_API_KEY
 
 def get_photo_urls(mountain_ids_names):
       resp = []
-   try: 
+      try: 
             for data in mountain_ids_names:
                   photoURL = get_photo_url(FLIRCK_API_KEY, data[0])
                   resp.append(data + (photoURL,))
-      return resp
+            return resp
 
-   except Exception as e:
-      print "Failed due to %s " % e
+      except Exception as e:
+            print "Failed due to %s " % e
             return []
 
 def post_nearest(lat, lng):
       qry = '''WITH subq AS (
-            SELECT name, feature, latitude, longitude, SQRT(
+            SELECT name, colheight, feature, latitude, longitude, SQRT(
                   POW(69.1 * (latitude - %f), 2) +
                   POW(69.1 * (%f - longitude) * COS(latitude / 57.3), 2)) AS distance
             FROM mountains)
       SELECT * FROM subq			
-      WHERE distance < 50 
+      WHERE distance < 200 AND colheight > 50
       ORDER BY distance LIMIT 8''' % (lat, lng)
 
       from sqlalchemy import text
       from project import db
-      return db.session.query('name', 'feature', 'latitude', 'longitude', 'distance').from_statement(text(qry)).all()
+      return db.session.query('name', 'colheight', 'feature', 'latitude', 'longitude', 'distance').from_statement(text(qry)).all()
 
